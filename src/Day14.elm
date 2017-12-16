5972d0f2491521afab71a2ddaf0c83cef28d4158
Simon Lydell <simon.lydell@gmail.com>
2017-12-16 09:48:03 +0100

    Day14 finished

--- src/Day14.elm
+++ src/Day14.elm
@@ -10,7 +10,7 @@ import Set exposing (Set)
 output : () -> ( String, String )
 output () =
     ( input |> makeGrid |> countY |> toString
-    , ""
+    , input |> makeGrid |> countClusters |> toString
     )
 
 
@@ -98,10 +98,9 @@ countY =
 
 
 countClusters : List (List Binary) -> Maybe Int
-countClusters list =
-    list
-        |> Matrix.fromList
-        |> Maybe.map (findAllClusters >> List.length)
+countClusters =
+    Matrix.fromList
+        >> Maybe.map (findAllClusters >> List.length)
 
 
 findAllClusters : Matrix Binary -> List (Set ( Int, Int ))
@@ -112,7 +111,15 @@ findAllClusters matrix =
                 clusters
 
             else
-                findCluster matrix coords :: clusters
+                let
+                    newCluster =
+                        findCluster matrix coords
+                in
+                if newCluster == Set.empty then
+                    clusters
+
+                else
+                    newCluster :: clusters
     in
     matrix
         |> Matrix.toIndexedArray
