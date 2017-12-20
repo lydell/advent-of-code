36294f51a54ff39d8107e8bf59be8953002d8e80
Simon Lydell <simon.lydell@gmail.com>
2017-12-20 08:12:53 +0100

    Day18 part 2

--- src/Day18.elm
+++ src/Day18.elm
@@ -7,19 +7,19 @@ import List.Extra
 
 output : () -> ( String, String )
 output () =
-    ( input |> parse |> firstRecovered |> toString
-    , ""
+    ( input |> parse |> firstReceived |> toString
+    , input |> parse |> duet |> Tuple.second |> .numSent |> toString
     )
 
 
 type Instruction
-    = Isnd String
+    = Isnd Value
     | Iset String Value
     | Iadd String Value
     | Imul String Value
     | Imod String Value
     | Ircv String
-    | Ijgz String Value
+    | Ijgz Value Value
 
 
 type Value
@@ -27,15 +27,39 @@ type Value
     | Number Int
 
 
-type Tone
-    = Silent
-    | Playing Int
-    | Recovering Int
+type alias Registers =
+    Dict String Int
 
 
-type alias Evaluation =
-    { registers : Dict String Int
-    , tone : Tone
+type alias Program =
+    { registers : Registers
+    , queue : List Int
+    , pos : Int
+    , numSent : Int
+    }
+
+
+type EvaluationResult
+    = Next Registers (List Int)
+    | Jump Int
+    | Send Int
+    | Receive (Maybe Int)
+    | Ignore
+
+
+type ProgramResult
+    = Updated Program
+    | Sending Int Program
+    | Receiving (Maybe Int)
+    | OutOfRange
+
+
+emptyProgram : Program
+emptyProgram =
+    { registers = Dict.empty
+    , queue = []
+    , pos = 0
+    , numSent = 0
     }
 
 
@@ -48,8 +72,8 @@ parse =
 parseLine : String -> Result String Instruction
 parseLine string =
     case String.words string of
-        [ "snd", register ] ->
-            Ok <| Isnd register
+        [ "snd", value ] ->
+            Ok <| Isnd (parseValue value)
 
         [ "set", register, value ] ->
             Ok <| Iset register (parseValue value)
@@ -66,8 +90,8 @@ parseLine string =
         [ "rcv", register ] ->
             Ok <| Ircv register
 
-        [ "jgz", register, value ] ->
-            Ok <| Ijgz register (parseValue value)
+        [ "jgz", value1, value2 ] ->
+            Ok <| Ijgz (parseValue value1) (parseValue value2)
 
         _ ->
             Err <| "Unknown/invalid operation: " ++ string
@@ -83,13 +107,17 @@ parseValue string =
             Register string
 
 
-emptyEvaluation : Evaluation
-emptyEvaluation =
-    { registers = Dict.empty, tone = Silent }
+safeModulo : Int -> Int -> Int
+safeModulo a b =
+    if b == 0 then
+        a
+
+    else
+        a % b
 
 
-evaluate : Instruction -> Evaluation -> ( Evaluation, Int )
-evaluate instruction ({ registers, tone } as evaluation) =
+evaluate : Instruction -> Registers -> List Int -> EvaluationResult
+evaluate instruction registers queue =
     let
         getRegister register =
             Dict.get register registers
@@ -103,128 +131,192 @@ evaluate instruction ({ registers, tone } as evaluation) =
                     Just number
 
         update f register value =
-            Maybe.map2
-                (\a b ->
-                    { evaluation
-                        | registers =
+            case ( getRegister register, getValue value ) of
+                ( Just a, Just b ) ->
+                    let
+                        newRegisters =
                             Dict.insert register (f a b) registers
-                    }
-                )
-                (getRegister register)
-                (getValue value)
-                |> Maybe.withDefault evaluation
+                    in
+                    Next newRegisters queue
 
-        safeModulo a b =
-            if b == 0 then
-                a
-
-            else
-                a % b
-
-        jumpOne result =
-            ( result, 1 )
+                _ ->
+                    Ignore
     in
     case instruction of
-        Isnd register ->
-            getRegister register
-                |> Maybe.map
-                    (\number ->
-                        { evaluation | tone = Playing number }
-                    )
-                |> Maybe.withDefault evaluation
-                |> jumpOne
+        Isnd value ->
+            case getValue value of
+                Just number ->
+                    Send number
+
+                Nothing ->
+                    Ignore
 
         Iset register value ->
-            getValue value
-                |> Maybe.map
-                    (\number ->
-                        { evaluation
-                            | registers = Dict.insert register number registers
-                        }
-                    )
-                |> Maybe.withDefault evaluation
-                |> jumpOne
+            case getValue value of
+                Just number ->
+                    let
+                        newRegisters =
+                            Dict.insert register number registers
+                    in
+                    Next newRegisters queue
+
+                Nothing ->
+                    Ignore
 
         Iadd register value ->
             update (+) register value
-                |> jumpOne
 
         Imul register value ->
             update (*) register value
-                |> jumpOne
 
         Imod register value ->
             update safeModulo register value
-                |> jumpOne
 
         Ircv register ->
-            getRegister register
-                |> Maybe.map
-                    (\number ->
-                        if number == 0 then
-                            evaluation
+            case queue of
+                [] ->
+                    Receive (getRegister register)
 
-                        else
-                            case tone of
-                                Silent ->
-                                    evaluation
+                number :: restQueue ->
+                    let
+                        newRegisters =
+                            Dict.insert register number registers
+                    in
+                    Next newRegisters restQueue
 
-                                Playing playingNumber ->
-                                    { evaluation
-                                        | tone = Recovering playingNumber
-                                    }
-
-                                Recovering _ ->
-                                    evaluation
-                    )
-                |> Maybe.withDefault evaluation
-                |> jumpOne
-
-        Ijgz register value ->
-            Maybe.map2
-                (\registerValue jumpValue ->
+        Ijgz value1 value2 ->
+            case ( getValue value1, getValue value2 ) of
+                ( Just registerValue, Just jumpValue ) ->
                     if registerValue > 0 then
-                        ( evaluation, jumpValue )
+                        Jump jumpValue
 
                     else
-                        ( evaluation, 1 )
-                )
-                (getRegister register)
-                (getValue value)
-                |> Maybe.withDefault (jumpOne evaluation)
+                        Ignore
+
+                _ ->
+                    Ignore
 
 
-evaluateFromList :
-    List Instruction
-    -> ( Evaluation, Int )
-    -> Maybe ( Evaluation, Int )
-evaluateFromList instructions ( evaluation, pos ) =
-    List.Extra.getAt pos instructions
-        |> Maybe.map (flip evaluate evaluation >> Tuple.mapSecond ((+) pos))
+updateProgram : List Instruction -> Program -> ProgramResult
+updateProgram instructions program =
+    case List.Extra.getAt program.pos instructions of
+        Just instruction ->
+            case evaluate instruction program.registers program.queue of
+                Next newRegisters newQueue ->
+                    Updated
+                        { program
+                            | registers = newRegisters
+                            , queue = newQueue
+                            , pos = program.pos + 1
+                        }
+
+                Jump jump ->
+                    Updated { program | pos = program.pos + jump }
+
+                Send number ->
+                    Sending number
+                        { program
+                            | numSent = program.numSent + 1
+                            , pos = program.pos + 1
+                        }
+
+                Receive currentValue ->
+                    Receiving currentValue
+
+                Ignore ->
+                    Updated { program | pos = program.pos + 1 }
+
+        Nothing ->
+            OutOfRange
 
 
-evaluateN : Int -> List Instruction -> Maybe ( Evaluation, Int )
-evaluateN n instructions =
-    List.foldl
-        (always (Maybe.andThen (evaluateFromList instructions)))
-        (Just ( emptyEvaluation, 0 ))
-        (List.repeat n ())
+firstReceived : List Instruction -> Maybe Int
+firstReceived instructions =
+    firstReceivedHelper instructions emptyProgram Nothing
 
 
-firstRecovered : List Instruction -> Maybe Int
-firstRecovered instructions =
-    firstRecoveredHelper instructions ( emptyEvaluation, 0 )
+firstReceivedHelper : List Instruction -> Program -> Maybe Int -> Maybe Int
+firstReceivedHelper instructions program lastSentNumber =
+    case updateProgram instructions program of
+        Updated newProgram ->
+            firstReceivedHelper instructions newProgram lastSentNumber
+
+        Sending number newProgram ->
+            firstReceivedHelper instructions newProgram (Just number)
+
+        Receiving currentValue ->
+            let
+                newProgram =
+                    { program | pos = program.pos + 1 }
+            in
+            case currentValue of
+                Just 0 ->
+                    firstReceivedHelper instructions newProgram lastSentNumber
+
+                Just _ ->
+                    lastSentNumber
+
+                Nothing ->
+                    firstReceivedHelper instructions newProgram lastSentNumber
+
+        OutOfRange ->
+            Nothing
 
 
-firstRecoveredHelper :
-    List Instruction
-    -> ( Evaluation, Int )
-    -> Maybe Int
-firstRecoveredHelper instructions ( evaluation, pos ) =
-    case evaluation.tone of
-        Recovering number ->
-            Just number
+updateDuetProgram : List Instruction -> Program -> ( Program, Maybe Int, Bool )
+updateDuetProgram instructions program =
+    case updateProgram instructions program of
+        Updated newProgram ->
+            ( newProgram, Nothing, True )
 
-        _ ->
-            evaluateFromList instructions ( evaluation, pos )
-                |> Maybe.andThen (firstRecoveredHelper instructions)
+        Sending number newProgram ->
+            ( newProgram, Just number, True )
+
+        Receiving _ ->
+            ( program, Nothing, False )
+
+        OutOfRange ->
+            ( program, Nothing, False )
+
+
+duet : List Instruction -> ( Program, Program )
+duet instructions =
+    let
+        program0 =
+            { emptyProgram | registers = Dict.singleton "p" 0 }
+
+        program1 =
+            { emptyProgram | registers = Dict.singleton "p" 1 }
+    in
+    duetHelper instructions ( program0, program1 )
+
+
+duetHelper : List Instruction -> ( Program, Program ) -> ( Program, Program )
+duetHelper instructions ( program0, program1 ) =
+    let
+        addToQueue maybeNumber program =
+            case maybeNumber of
+                Just number ->
+                    { program | queue = program.queue ++ [ number ] }
+
+                Nothing ->
+                    program
+
+        ( newProgram0, sent0, running0 ) =
+            updateDuetProgram instructions program0
+
+        ( newProgram1, sent1, running1 ) =
+            updateDuetProgram instructions program1
+
+        finalProgram0 =
+            addToQueue sent1 newProgram0
+
+        finalProgram1 =
+            addToQueue sent0 newProgram1
+    in
+    if not running0 && not running1 then
+        ( finalProgram0, finalProgram1 )
+
+    else
+        -- ( finalProgram0, finalProgram1 )
+        duetHelper instructions ( finalProgram0, finalProgram1 )
--- tests/TestDay18.elm
+++ tests/TestDay18.elm
@@ -1,12 +1,12 @@
 module TestDay18 exposing (..)
 
-import Day18 exposing (firstRecovered, parse)
+import Day18 exposing (duet, firstReceived, parse)
 import Expect exposing (Expectation)
 import Test exposing (..)
 
 
-input : String
-input =
+input1 : String
+input1 =
     """set a 1
 add a 2
 mul a a
@@ -19,13 +19,59 @@ set a 1
 jgz a -2"""
 
 
+input2 : String
+input2 =
+    """snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d"""
+
+
+input3 : String
+input3 =
+    """set s 1
+set t 2
+snd s
+jgz p 5
+
+rcv a
+rcv a
+add t 2
+rcv a
+
+mul t -1
+mod t 0
+snd t"""
+
+
 suite : Test
 suite =
     describe "Day18"
-        [ describe "firstRecovered"
+        [ describe "firstReceived"
             [ test "it works" <|
                 \_ ->
-                    firstRecovered (parse input)
+                    firstReceived (parse input1)
                         |> Expect.equal (Just 4)
+            ]
+        , describe "duet"
+            [ test "provided example" <|
+                \_ ->
+                    let
+                        ( program0, program1 ) =
+                            input2 |> parse |> duet
+                    in
+                    ( program0.numSent, program1.numSent )
+                        |> Expect.equal ( 3, 3 )
+            , test "input3" <|
+                \_ ->
+                    let
+                        ( program0, program1 ) =
+                            input3 |> parse |> duet
+                    in
+                    ( program0.numSent, program1.numSent )
+                        |> Expect.equal ( 1, 2 )
             ]
         ]
