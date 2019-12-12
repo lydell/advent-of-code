ca74cb5d917492be1d3baecd2e5e17451787a41f
Simon Lydell <simon.lydell@gmail.com>
2019-12-13 00:10:28 +0100

    Mouse

--- src/Day3.elm
+++ src/Day3.elm
@@ -6,9 +6,11 @@ import Browser.Events
 import Day3Input exposing (puzzleInput)
 import Html exposing (Html)
 import Html.Attributes as Attr
-import Html.Events as Events
+import Html.Events
+import Json.Decode
 import Svg exposing (Svg)
 import Svg.Attributes as SvgAttr
+import Svg.Events
 import Task exposing (Task)
 
 
@@ -142,17 +144,24 @@ main =
 type alias Model =
     { input : String
     , parsed : ( List Wire, Bounds )
-    , svgViewport : Browser.Dom.Viewport
+    , boundsElement : Browser.Dom.Element
+    , mouse : Maybe ( Float, Float )
     }
 
 
-emptyViewport : Browser.Dom.Viewport
-emptyViewport =
+emptyElement : Browser.Dom.Element
+emptyElement =
     { scene =
         { width = 0
         , height = 0
         }
     , viewport =
+        { x = 0
+        , y = 0
+        , width = 0
+        , height = 0
+        }
+    , element =
         { x = 0
         , y = 0
         , width = 0
@@ -169,32 +178,41 @@ init =
     in
     ( { input = input
       , parsed = parse input
-      , svgViewport = emptyViewport
+      , boundsElement = emptyElement
+      , mouse = Nothing
       }
-    , getSvgViewport
+    , getBoundsElement
     )
 
 
 type Msg
     = InputChanged String
     | WindowResized
-    | GotSvgViewport (Result Browser.Dom.Error Browser.Dom.Viewport)
+    | GotBoundsElement (Result Browser.Dom.Error Browser.Dom.Element)
+    | MouseMove Float Float
+    | MouseOut
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         InputChanged text ->
-            ( { model | input = text, parsed = parse text }, Cmd.none )
+            ( { model | input = text, parsed = parse text }, getBoundsElement )
 
         WindowResized ->
-            ( model, getSvgViewport )
+            ( model, getBoundsElement )
 
-        GotSvgViewport (Ok viewport) ->
-            ( { model | svgViewport = viewport }, Cmd.none )
+        GotBoundsElement (Ok element) ->
+            ( { model | boundsElement = element }, Cmd.none )
 
-        GotSvgViewport (Err _) ->
+        GotBoundsElement (Err _) ->
             ( model, Cmd.none )
+
+        MouseMove x y ->
+            ( { model | mouse = Just ( x, y ) }, Cmd.none )
+
+        MouseOut ->
+            ( { model | mouse = Nothing }, Cmd.none )
 
 
 svgId : String
@@ -202,10 +220,10 @@ svgId =
     "svg"
 
 
-getSvgViewport : Cmd Msg
-getSvgViewport =
-    Browser.Dom.getViewportOf svgId
-        |> Task.attempt GotSvgViewport
+getBoundsElement : Cmd Msg
+getBoundsElement =
+    Browser.Dom.getElement svgId
+        |> Task.attempt GotBoundsElement
 
 
 view : Model -> Html Msg
@@ -221,7 +239,19 @@ view model =
             max 1 (bounds.bottom - bounds.top)
 
         svgWidth =
-            model.svgViewport.viewport.width
+            max 1 model.boundsElement.element.width
+
+        svgHeight =
+            max 1 model.boundsElement.element.height
+
+        mouse =
+            Maybe.map
+                (\( x, y ) ->
+                    ( bounds.left + round ((x - model.boundsElement.element.x) / svgWidth * toFloat width)
+                    , bounds.top + round ((y - model.boundsElement.element.y) / svgHeight * toFloat height)
+                    )
+                )
+                model.mouse
 
         viewBox =
             [ bounds.left, bounds.top, width, height ]
@@ -248,7 +278,13 @@ view model =
             , Attr.style "position" "relative"
             , Attr.style "margin-right" "30px"
             ]
-            [ Svg.svg
+            [ case mouse of
+                Just ( x, y ) ->
+                    Html.text (String.fromInt (abs x + abs y))
+
+                Nothing ->
+                    Html.text ""
+            , Svg.svg
                 [ Attr.style "position" "absolute"
                 , Attr.style "top" "0"
                 , Attr.style "left" "0"
@@ -256,10 +292,8 @@ view model =
                 , Attr.style "height" "100%"
                 , Attr.style "overflow" "visible"
                 , SvgAttr.viewBox viewBox
-                , SvgAttr.id svgId
                 ]
                 [ Svg.g [] wiresSvg
-                , Svg.path [ SvgAttr.d "M0,0 Z", SvgAttr.stroke "#fff", SvgAttr.strokeWidth "10" ] []
                 , Svg.circle
                     [ SvgAttr.cx "0"
                     , SvgAttr.cy "0"
@@ -270,12 +304,38 @@ view model =
                     , Attr.attribute "vector-effect" "non-scaling-stroke"
                     ]
                     []
+                , case mouse of
+                    Just ( x, y ) ->
+                        Svg.circle
+                            [ SvgAttr.cx (String.fromInt x)
+                            , SvgAttr.cy (String.fromInt y)
+                            , SvgAttr.r (String.fromFloat (8 * toFloat width / svgWidth))
+                            , SvgAttr.fill "none"
+                            , SvgAttr.stroke "#fff"
+                            , SvgAttr.strokeWidth "4"
+                            , Attr.attribute "vector-effect" "non-scaling-stroke"
+                            ]
+                            []
+
+                    Nothing ->
+                        Svg.text ""
+                , Svg.rect
+                    [ SvgAttr.x (String.fromInt bounds.left)
+                    , SvgAttr.y (String.fromInt bounds.top)
+                    , SvgAttr.width (String.fromInt width)
+                    , SvgAttr.height (String.fromInt height)
+                    , SvgAttr.fill "transparent"
+                    , SvgAttr.id svgId
+                    , Svg.Events.on "mousemove" mouseMoveDecoder
+                    , Svg.Events.onMouseOut MouseOut
+                    ]
+                    []
                 ]
             ]
         , Html.textarea
             [ Attr.style "width" "400px"
             , Attr.style "padding" "5px"
-            , Events.onInput InputChanged
+            , Html.Events.onInput InputChanged
             , Attr.value model.input
             ]
             []
@@ -309,6 +369,13 @@ viewWire color wire =
         , SvgAttr.strokeLinejoin "round"
         ]
         []
+
+
+mouseMoveDecoder : Json.Decode.Decoder Msg
+mouseMoveDecoder =
+    Json.Decode.map2 MouseMove
+        (Json.Decode.field "pageX" Json.Decode.float)
+        (Json.Decode.field "pageY" Json.Decode.float)
 
 
 subscriptions : Model -> Sub Msg
