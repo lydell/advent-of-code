6f45cedad20509c8448fa1046c44292c40367d17
Simon Lydell <simon.lydell@gmail.com>
2020-12-04 12:48:13 +0100

    Day 4 part 2

--- src/Day4.elm
+++ src/Day4.elm
@@ -2,6 +2,7 @@ module Day4 exposing (..)
 
 import Day4Input exposing (puzzleInput)
 import Html exposing (Html)
+import Regex exposing (Regex)
 import Set exposing (Set)
 
 
@@ -32,22 +33,48 @@ solution1 =
         >> List.length
 
 
+solution2 : String -> Int
+solution2 =
+    String.trim
+        >> String.split "\n\n"
+        >> List.filter isValid
+        >> List.length
+
+
+isValid : String -> Bool
+isValid input =
+    List.all (\regex -> Regex.contains regex input) regexes
+
+
+regexes : List Regex
+regexes =
+    [ "byr:(19[2-9]\\d|19[4-9]\\d|200[0-2])"
+    , "iyr:(201\\d|2020)"
+    , "eyr:(202\\d|2030)"
+    , "hgt:((1[5-8]\\d|19[0-3])cm|(59|6\\d|7[0-6])in)"
+    , "hcl:#[\\da-f]{6}"
+    , "ecl:(amb|blu|brn|gry|grn|hzl|oth)"
+    , "pid:\\d{9}"
+    ]
+        |> List.map
+            (\string ->
+                "(^|\\s)"
+                    ++ string
+                    ++ "(\\s|$)"
+                    |> Regex.fromString
+                    |> Maybe.withDefault Regex.never
+            )
+
+
 main : Html Never
 main =
     Html.div []
-        [ showResult (solution1 puzzleInput |> Ok)
+        [ showResult (solution1 puzzleInput)
+        , showResult (solution2 puzzleInput)
         ]
 
 
-showResult : Result String Int -> Html msg
+showResult : Int -> Html msg
 showResult result =
     Html.output []
-        [ Html.text
-            (case result of
-                Ok int ->
-                    String.fromInt int
-
-                Err error ->
-                    error
-            )
-        ]
+        [ Html.text (String.fromInt result) ]
