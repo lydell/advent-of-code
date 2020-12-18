92a66eaa176ccd8e9807331e8be8c16ade21b4d4
Simon Lydell <simon.lydell@gmail.com>
2020-12-18 09:17:54 +0100

    Day 18

---
+++ src/Parser/Extra.elm
@@ -0,0 +1,62 @@
+-- Copied from https://github.com/elm/parser/pull/16
+
+
+module Parser.Extra exposing (deadEndsToString)
+
+import Parser
+
+
+deadEndsToString : List Parser.DeadEnd -> String
+deadEndsToString deadEnds =
+    String.concat (List.intersperse "; " (List.map deadEndToString deadEnds))
+
+
+deadEndToString : Parser.DeadEnd -> String
+deadEndToString deadend =
+    problemToString deadend.problem ++ " at row " ++ String.fromInt deadend.row ++ ", col " ++ String.fromInt deadend.col
+
+
+problemToString : Parser.Problem -> String
+problemToString p =
+    case p of
+        Parser.Expecting s ->
+            "expecting '" ++ s ++ "'"
+
+        Parser.ExpectingInt ->
+            "expecting int"
+
+        Parser.ExpectingHex ->
+            "expecting hex"
+
+        Parser.ExpectingOctal ->
+            "expecting octal"
+
+        Parser.ExpectingBinary ->
+            "expecting binary"
+
+        Parser.ExpectingFloat ->
+            "expecting float"
+
+        Parser.ExpectingNumber ->
+            "expecting number"
+
+        Parser.ExpectingVariable ->
+            "expecting variable"
+
+        Parser.ExpectingSymbol s ->
+            "expecting symbol '" ++ s ++ "'"
+
+        Parser.ExpectingKeyword s ->
+            "expecting keyword '" ++ s ++ "'"
+
+        Parser.ExpectingEnd ->
+            "expecting end"
+
+        Parser.UnexpectedChar ->
+            "unexpected char"
+
+        Parser.Problem s ->
+            "problem " ++ s
+
+        Parser.BadRepeat ->
+            "bad repeat"
