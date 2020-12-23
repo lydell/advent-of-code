d67b771e7454a69d039f8c603d8e8b1190979247
Simon Lydell <simon.lydell@gmail.com>
2020-12-23 23:19:31 +0100

    Day 23 part 2 working

--- src/Day23.elm
+++ src/Day23.elm
@@ -1,143 +1,174 @@
+-- Warning: Part 2 took like one and a half minutes to complete on my machine!
+
+
 module Day23 exposing (..)
 
 import Day23Input exposing (puzzleInput)
-import Deque exposing (Deque)
+import Dict exposing (Dict)
 import Html exposing (Html)
+import IntDict exposing (after)
+import List.Extra
 
 
-solution1 : List Int -> String
-solution1 cups =
-    let
-        maximum =
-            List.maximum cups |> Maybe.withDefault 0
-    in
-    List.repeat 100 ()
-        |> List.foldl (always (doOneRound maximum)) (Deque.fromList cups)
-        |> getOrderString
+solution1 : Parsed -> Result String Int
+solution1 { maximum, first, cups } =
+    doRounds 100 maximum ( first, cups )
+        |> Result.andThen getOrderInt
 
 
-getOrderString : Deque Int -> String
-getOrderString cups =
-    let
-        ( before, _, after ) =
-            getDestination 1 1 cups
-    in
-    after
-        |> append before
-        |> Deque.map String.fromInt
-        |> Deque.toList
+getOrderInt : Dict Int Int -> Result String Int
+getOrderInt cups =
+    getOrderIntHelper 1 cups []
+        |> List.map String.fromInt
         |> String.join ""
+        |> String.toInt
+        |> Result.fromMaybe "Not an integer"
 
 
-append : Deque a -> Deque a -> Deque a
-append a b =
-    Deque.append b a
-
-
-doOneRound : Int -> Deque Int -> Deque Int
-doOneRound maximum cups =
-    case Deque.popFront cups of
-        ( Just first, rest ) ->
-            let
-                pickedUp =
-                    rest |> Deque.left 3
-
-                ( before, destination, after ) =
-                    getDestination maximum
-                        (first - 1)
-                        (Deque.dropLeft (Deque.length pickedUp) rest)
-
-                -- _ =
-                --     Debug.log "x" ( first, destination )
-                -- _ =
-                --     Debug.log "dest" ( Deque.length before, destination, Deque.length after )
-            in
-            before
-                |> Deque.pushBack destination
-                |> append pickedUp
-                |> append after
-                |> Deque.pushBack first
-
-        ( Nothing, _ ) ->
-            cups
-
-
-getDestination : Int -> Int -> Deque Int -> ( Deque Int, Int, Deque Int )
-getDestination maximum target rest =
-    getDestinationHelper maximum target rest Deque.empty
-
-
-getDestinationHelper : Int -> Int -> Deque Int -> Deque Int -> ( Deque Int, Int, Deque Int )
-getDestinationHelper maximum target left right =
-    case Deque.popBack left of
-        ( Just last, rest ) ->
-            if last == target then
-                ( rest, last, right )
+getOrderIntHelper : Int -> Dict Int Int -> List Int -> List Int
+getOrderIntHelper current cups result =
+    case Dict.get current cups of
+        Just next ->
+            if next == 1 then
+                List.reverse result
 
             else
-                getDestinationHelper maximum target rest (right |> Deque.pushFront last)
+                getOrderIntHelper next cups (next :: result)
 
-        ( Nothing, _ ) ->
+        Nothing ->
+            List.reverse result
+
+
+doRounds : Int -> Int -> ( Int, Dict Int Int ) -> Result String (Dict Int Int)
+doRounds rounds maximum currentAndCups =
+    case doOneRound maximum currentAndCups of
+        Ok next ->
+            if rounds > 1 then
+                doRounds (rounds - 1) maximum next
+
+            else
+                Ok (Tuple.second next)
+
+        Err message ->
+            Err message
+
+
+doOneRound : Int -> ( Int, Dict Int Int ) -> Result String ( Int, Dict Int Int )
+doOneRound maximum ( current, cups ) =
+    case getHead current cups of
+        Ok ( next1, next2, ( next3, next4 ) ) ->
             let
-                nextTarget =
-                    if target <= 1 then
-                        maximum
-
-                    else
-                        target - 1
+                destination =
+                    getDestination maximum current [ next1, next2, next3 ]
             in
-            getDestinationHelper maximum nextTarget right Deque.empty
+            case Dict.get destination cups of
+                Just afterDestination ->
+                    let
+                        nextCups =
+                            cups
+                                |> Dict.insert current next4
+                                |> Dict.insert destination next1
+                                |> Dict.insert next3 afterDestination
+                    in
+                    Ok ( next4, nextCups )
+
+                Nothing ->
+                    Err "destination not found"
+
+        Err message ->
+            Err message
 
 
-solution2 : List Int -> String
-solution2 initialCups =
+getHead : Int -> Dict Int Int -> Result String ( Int, Int, ( Int, Int ) )
+getHead current cups =
+    case Dict.get current cups of
+        Just next1 ->
+            case Dict.get next1 cups of
+                Just next2 ->
+                    case Dict.get next2 cups of
+                        Just next3 ->
+                            case Dict.get next3 cups of
+                                Just next4 ->
+                                    Ok ( next1, next2, ( next3, next4 ) )
+
+                                Nothing ->
+                                    Err "next3 not found"
+
+                        Nothing ->
+                            Err "next2 not found"
+
+                Nothing ->
+                    Err "next1 not found"
+
+        Nothing ->
+            Err "current not found"
+
+
+getDestination : Int -> Int -> List Int -> Int
+getDestination maximum current pickedUp =
     let
-        max =
-            initialCups
-                |> List.maximum
-                |> Maybe.withDefault 0
+        target =
+            if current <= 1 then
+                maximum
 
-        maximum =
-            1000000
-
-        cups =
-            (initialCups ++ List.range (max + 1) maximum)
-                |> Deque.fromList
+            else
+                current - 1
     in
-    List.range 1 10000000
-        |> List.foldl (log >> always (doOneRound maximum)) cups
-        |> getStarsProduct
-
-
-log n =
-    if n - 1 |> modBy 10 |> (==) 0 then
-        Debug.log "n" n
+    if List.member target pickedUp then
+        getDestination maximum target pickedUp
 
     else
-        n
+        target
 
 
-getStarsProduct : Deque Int -> String
-getStarsProduct cups =
+solution2 : Parsed -> Result String Int
+solution2 { maximum, first, last, cups } =
     let
-        ( before, _, after ) =
-            getDestination 1 1 cups
+        newMaximum =
+            1000000
+
+        fullCups =
+            List.range (maximum + 1) (newMaximum - 1)
+                |> List.foldl (\n -> Dict.insert n (n + 1)) cups
+                |> Dict.insert last (maximum + 1)
+                |> Dict.insert newMaximum first
     in
-    after
-        |> Deque.left 2
-        |> Deque.toList
-        |> List.product
-        |> String.fromInt
+    doRounds 10000000 newMaximum ( first, fullCups )
+        |> Result.andThen getStarsProduct
+
+
+getStarsProduct : Dict Int Int -> Result String Int
+getStarsProduct cups =
+    case Dict.get 1 cups of
+        Just next1 ->
+            case Dict.get next1 cups of
+                Just next2 ->
+                    Ok (next1 * next2)
+
+                Nothing ->
+                    Err "next1 not found"
+
+        Nothing ->
+            Err "1 not found"
 
 
 main : Html Never
 main =
     Html.div []
-        -- [ Html.text (solution1 puzzleInput)
-        [ Html.text (solution2 puzzleInput)
+        [ showResult (solution1 puzzleInput)
+        , showResult (solution2 puzzleInput)
         ]
 
 
-shortInput : List Int
-shortInput =
-    [ 3, 8, 9, 1, 2, 5, 4, 6, 7 ]
+showResult : Result String Int -> Html msg
+showResult result =
+    Html.output []
+        [ Html.text
+            (case result of
+                Ok int ->
+                    String.fromInt int
+
+                Err error ->
+                    error
+            )
+        ]
