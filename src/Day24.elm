7425e03cb8b3df6d07c272c9700637e12b99a854
Simon Lydell <simon.lydell@gmail.com>
2020-12-24 10:57:23 +0100

    Day 24 part 1

---
+++ src/Day24.elm
@@ -0,0 +1,105 @@
+module Day24 exposing (..)
+
+import Day24Input exposing (puzzleInput)
+import Hexagons.Hex as Hex exposing (Direction, Hex)
+import Html exposing (Html)
+import LineParser
+import Regex exposing (Regex)
+import Set
+
+
+regex : Regex
+regex =
+    Regex.fromString "e|se|sw|w|nw|ne|."
+        |> Maybe.withDefault Regex.never
+
+
+parse : String -> Result String (List (List Direction))
+parse =
+    LineParser.parse
+        (Regex.find regex
+            >> List.map .match
+            >> LineParser.parseGeneral "Direction"
+                identity
+                (\direction ->
+                    case direction of
+                        "e" ->
+                            Ok Hex.E
+
+                        "se" ->
+                            Ok Hex.SE
+
+                        "sw" ->
+                            Ok Hex.SW
+
+                        "w" ->
+                            Ok Hex.W
+
+                        "nw" ->
+                            Ok Hex.NW
+
+                        "ne" ->
+                            Ok Hex.NE
+
+                        _ ->
+                            Err ("Unknown direction: " ++ direction)
+                )
+        )
+
+
+solution1 : String -> Result String Int
+solution1 =
+    parse >> Result.map solve1
+
+
+solve1 : List (List Direction) -> Int
+solve1 =
+    List.foldl
+        (\directions blacks ->
+            let
+                next =
+                    List.foldl
+                        (\direction hex ->
+                            Hex.neighbor hex direction
+                        )
+                        (Hex.intFactory ( 0, 0 ))
+                        directions
+                        |> hexToTriple
+            in
+            if Set.member next blacks then
+                Set.remove next blacks
+
+            else
+                Set.insert next blacks
+        )
+        Set.empty
+        >> Set.size
+
+
+hexToTriple : Hex -> ( Int, Int, Int )
+hexToTriple hex =
+    ( Hex.intQ hex
+    , Hex.intR hex
+    , Hex.intS hex
+    )
+
+
+main : Html Never
+main =
+    Html.div []
+        [ showResult (solution1 puzzleInput)
+        ]
+
+
+showResult : Result String Int -> Html msg
+showResult result =
+    Html.output []
+        [ Html.text
+            (case result of
+                Ok int ->
+                    String.fromInt int
+
+                Err error ->
+                    error
+            )
+        ]
