afd8f4bb6674838d5477a9a07ec7daf3a1145950
Simon Lydell <simon.lydell@gmail.com>
2020-12-25 16:00:12 +0100

    Day 25 cleanup

--- src/Day25.elm
+++ src/Day25.elm
@@ -4,49 +4,38 @@ import Day25Input exposing (puzzleInput)
 import Html exposing (Html)
 
 
-solution1 : Int -> ( Int, Int ) -> Result String Int
-solution1 s ( first, second ) =
-    { --first = ( first, findLoopSize 7 first )
-      -- , second = ( second, findSubjectNumberAndLoopSize second )
-      --   first = ( first, transformAtMost 100000000 7 first ( 1, 0 ) )
-      -- , second = ( second, transformAtMost 100000000 7 second ( 1, 0 ) )
-      enc = transformAtMost s first -1 ( 1, 0 )
-
-    -- , tmash2 = List.repeat 11 () |> List.foldl (always (transform 7)) 1
-    -- , tmash3 = List.range 1 20 |> List.map (\n -> List.repeat n () |> List.foldl (always (transform 7)) 1)
-    }
-        |> Debug.toString
-        |> Err
+type LoopSize
+    = First Int
+    | Second Int
 
 
-findSubjectNumberAndLoopSize : Int -> ( Int, Int )
-findSubjectNumberAndLoopSize target =
-    findSubjectNumberAndLoopSizeHelper target 2
-
-
-findSubjectNumberAndLoopSizeHelper : Int -> Int -> ( Int, Int )
-findSubjectNumberAndLoopSizeHelper target currentSubjectNumber =
+solution : ( Int, Int ) -> Int
+solution ( first, second ) =
     let
-        ( final, count ) =
-            transformAtMost 5 currentSubjectNumber target ( 1, 0 )
+        ( subjectNumber, loopSize ) =
+            case findLoopSize ( first, second ) ( 1, 0 ) of
+                First count ->
+                    ( second, count )
+
+                Second count ->
+                    ( first, count )
     in
-    if final == target then
-        ( currentSubjectNumber, count )
+    List.repeat loopSize ()
+        |> List.foldl
+            (always (transform subjectNumber))
+            1
+
+
+findLoopSize : ( Int, Int ) -> ( Int, Int ) -> LoopSize
+findLoopSize ( first, second ) ( n, count ) =
+    if first == n then
+        First count
+
+    else if second == n then
+        Second count
 
     else
-        findSubjectNumberAndLoopSizeHelper target (currentSubjectNumber + 1)
-
-
-transformAtMost : Int -> Int -> Int -> ( Int, Int ) -> ( Int, Int )
-transformAtMost maxCount subjectNumber target ( n, count ) =
-    if n == target then
-        ( n, count )
-
-    else if count >= maxCount then
-        ( n, count )
-
-    else
-        transformAtMost maxCount subjectNumber target ( transform subjectNumber n, count + 1 )
+        findLoopSize ( first, second ) ( transform 7 n, count + 1 )
 
 
 transform : Int -> Int -> Int
@@ -57,26 +46,18 @@ transform subjectNumber n =
 main : Html Never
 main =
     Html.div []
-        [ showResult (solution1 11 exampleInput)
-        , showResult (solution1 8516638 puzzleInput)
+        [ showResult (solution exampleInput)
+        , showResult (solution puzzleInput)
         ]
 
 
-showResult : Result String Int -> Html msg
+showResult : Int -> Html msg
 showResult result =
     Html.output []
-        [ Html.text
-            (case result of
-                Ok int ->
-                    String.fromInt int
-
-                Err error ->
-                    error
-            )
+        [ Html.text (String.fromInt result)
         ]
 
 
 exampleInput : ( Int, Int )
 exampleInput =
-    -- (card, door)
     ( 5764801, 17807724 )
