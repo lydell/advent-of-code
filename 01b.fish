812b43430f6602fff572646cd53c36424130f381
Simon Lydell <simon.lydell@gmail.com>
2021-12-03 22:04:49 +0100

    Simplify Day 1 part 2

--- 01b.fish
+++ 01b.fish
@@ -1,29 +1,15 @@
 set count 0
-set i 0
-set a1 0
-set a2 0
-set a3 0
-set a4 0
-set b1 0
-set b2 0
-set b3 0
+set v1 0
+set v2 0
+set v3 0
+set v4 0
 while read line
-    set a1 $a2
-    set a2 $a3
-    set a3 $a4
-    set a4 $line
-    if test $i -gt 0
-        set b1 $b2
-        set b2 $b3
-        set b3 $line
+    set v1 $v2
+    set v2 $v3
+    set v3 $v4
+    set v4 $line
+    if test $v1 -gt 0 && test $v4 -gt $v1
+        set count (math $count + 1)
     end
-    if test $i -gt 2
-        set a (math $a1 + $a2 + $a3)
-        set b (math $b1 + $b2 + $b3)
-        if test $b -gt $a
-            set count (math $count + 1)
-        end
-    end
-    set i (math $i + 1)
 end
 echo $count
