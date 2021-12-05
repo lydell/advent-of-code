6bffcaff7ddbb60220df0fbe822ec39abe9e3696
Simon Lydell <simon.lydell@gmail.com>
2021-12-05 10:17:04 +0100

    Day 5 part 2

--- 05a.fish
+++ 05a.fish
@@ -1,7 +1,14 @@
+set part $argv[1]
+if test (count $part) = 0
+    set part a
+end
+
 rg ',' -r ' ' | while read x1 y1 _ x2 y2
     if test $x1 = $x2
         echo $x1:(seq $y1 $y2)
     else if test $y1 = $y2
         echo (seq $x1 $x2):$y1
+    else if test $part = b
+        paste -d : (seq $x1 $x2 | psub) (seq $y1 $y2 | psub)
     end
 end | string split ' ' | sort | uniq -c | string match -vr '^\s*1\s' | wc -l | string trim
