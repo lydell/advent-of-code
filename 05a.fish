6ab780d7a1e16397bf9181e2d93a5c4810d72875
Simon Lydell <simon.lydell@gmail.com>
2021-12-05 09:42:38 +0100

    Day 5 part 1, fast version

--- 05a.fish
+++ 05a.fish
@@ -1,37 +1,7 @@
-set count 0
-set once
-set twice
-set i 0
 rg ',' -r ' ' | while read x1 y1 _ x2 y2
-    set i (math $i + 1)
-    set j 0
-    echo $i
     if test $x1 = $x2
-        for y in (seq $y1 $y2)
-            set j (math $j + 1)
-            echo $i:$j
-            set item $x1:$y
-            if not contains $item $twice
-                if not contains $item $once
-                    set once $once $item
-                else
-                    set twice $twice $item
-                end
-            end
-        end
+        echo $x1:(seq $y1 $y2)
     else if test $y1 = $y2
-        for x in (seq $x1 $x2)
-            set j (math $j + 1)
-            echo $i:$j
-            set item $x:$y1
-            if not contains $item $twice
-                if not contains $item $once
-                    set once $once $item
-                else
-                    set twice $twice $item
-                end
-            end
-        end
+        echo (seq $x1 $x2):$y1
     end
-end
-count $twice
+end | string split ' ' | sort | uniq -c | string match -vr '^\s*1\s' | wc -l | string trim
