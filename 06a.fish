0b4d394e07104e54d68cc98c4c8264f0af8fc9bf
Simon Lydell <simon.lydell@gmail.com>
2021-12-06 09:55:25 +0100

    Day 6 part 1, fast version, and part 2

--- 06a.fish
+++ 06a.fish
@@ -1,19 +1,38 @@
-set fish (cat | string split ,)
-
-set count (count $fish)
-
-for day in (seq 80)
-    echo Starting day $day with $count fish
-    for i in (seq (count $fish))
-        set n $fish[$i]
-        if test $n = 0
-            set fish[$i] 6
-            set count (math $count + 1)
-            set fish[$count] 8
-        else
-            set fish[$i] (math $n - 1)
-        end
-    end
+set num_days $argv[1]
+if test (count $num_days) = 0
+    set num_days 80
 end
 
-echo $count
+function sum
+    set s 0
+    for x in $argv
+        set s (math $s + $x)
+    end
+    echo $s
+end
+
+set empty_fish 0 0 0 0 0 0 0 0 0
+set fish $empty_fish
+
+cat | string split , | while read age
+    set i (math $age + 1)
+    set fish[$i] (math $fish[$i] + 1)
+end
+
+for day in (seq $num_days)
+    set new_fish $empty_fish
+    for i in (seq (count $fish))
+        set age (math $i - 1)
+        set count (math $fish[$i])
+        if test $age = 0
+            set new_fish[7] $count
+            set new_fish[9] $count
+        else
+            set j (math $i - 1)
+            set new_fish[$j] (math $new_fish[$j] + $count)
+        end
+    end
+    set fish $new_fish
+end
+
+sum $fish
---
+++ 06b.fish
@@ -0,0 +1,1 @@
+fish (status dirname)/06a.fish 256
