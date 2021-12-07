4c1c274ea84f8bae0b6c20749eb630871e6c53f2
Simon Lydell <simon.lydell@gmail.com>
2021-12-07 06:55:53 +0100

    Optimize Day 7, part 1 from >3m to 0.5s

--- 07a.fish
+++ 07a.fish
@@ -1,19 +1,22 @@
 set positions (cat | string split , | sort -h)
 set low $positions[1]
 set high $positions[-1]
+set length (count $positions)
 
-set smallest
+set fuel 0
+for pos in $positions
+    set fuel (math "$fuel + abs($pos - $low)")
+end
 
-for i in (seq $low $high)
-    echo $i
-    set fuel 0
-    for pos in $positions
-        set fuel (math "$fuel + abs($pos - $i)")
-        if test (count $smallest) = 1 && test $fuel -gt $smallest
-            break
-        end
+set pos_index 1
+set smallest $fuel
+
+for i in (seq (math $low + 1) $high)
+    while test $positions[$pos_index] -lt $i
+        set pos_index (math $pos_index + 1)
     end
-    if test (count $smallest) = 0 || test $fuel -lt $smallest
+    set fuel (math "$fuel + 2 * ($pos_index - 1) - $length")
+    if test $fuel -lt $smallest
         set smallest $fuel
     end
 end
