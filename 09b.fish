4d3f9fe092b5882f4fcd4768eaf1dd1baf45b75b
Simon Lydell <simon.lydell@gmail.com>
2021-12-09 07:07:23 +0100

    Day 9 part 2

---
+++ 09b.fish
@@ -0,0 +1,43 @@
+set raw_lines (cat)
+set raw_first_line $raw_lines[1]
+set width (string length $raw_first_line)
+set height (count $raw_lines)
+
+set nines (string replace -ar '.' 9 $raw_first_line)
+set lines $nines $raw_lines $nines
+set lines (string replace -ar '^|$' 9 $lines)
+
+set seen_basin_coords
+function get_basin -a last_n x y
+    set item $x:$y
+    set line $lines[$y]
+    set n (string sub -s $x -l 1 $line)
+    if test $n = 9 || test $n -lt $last_n || contains $item $seen_basin_coords
+        echo 0
+    else
+        set -a seen_basin_coords $item
+        math 1 + (get_basin $n $x (math $y - 1)) + (get_basin $n (math $x + 1) $y) + (get_basin $n $x (math $y + 1)) + (get_basin $n (math $x - 1) $y)
+    end
+end
+
+set previous_line
+set line (string split '' $lines[1])
+set next_line (string split '' $lines[2])
+
+set basins
+
+for y in (seq 2 (math 1 + $height))
+    set -l previous_line $line
+    set -l line $next_line
+    set -l next_line (string split '' $lines[(math $y + 1)])
+    for x in (seq 2 (math 1 + $width))
+        set -l n $line[$x]
+        if test $n -lt $previous_line[$x] && test $n -lt $line[(math $x + 1)] && test $n -lt $next_line[$x] && test $n -lt $line[(math $x - 1)]
+            set seen_basin_coords
+            set -a basins (get_basin 0 $x $y)
+        end
+    end
+end
+
+set sorted_basins (string join \n $basins | sort -hr)
+math $sorted_basins[1] \* $sorted_basins[2] \* $sorted_basins[3]
