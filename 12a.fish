271d9303ba3149fe00d83a11a8c820dba89f198e
Simon Lydell <simon.lydell@gmail.com>
2021-12-12 07:36:25 +0100

    Day 12 part 2, but it takes 4m 30s

--- 12a.fish
+++ 12a.fish
@@ -1,3 +1,8 @@
+set initial_allow_visit_one_small_cave_twice $argv[1]
+if test (count $initial_allow_visit_one_small_cave_twice) = 0
+    set initial_allow_visit_one_small_cave_twice false
+end
+
 function is_upper -a s
     test $s = (string upper $s)
 end
@@ -9,20 +14,22 @@ end
 
 set count 0
 
-function walk
+function walk -a allow_visit_one_small_cave_twice
     set current $argv[-1]
     if test $current = end
-        string join , $argv
+        string join , $argv[2..]
         set count (math $count + 1)
     else
         for next in $$current
             if is_upper $next || not contains $next $argv
                 walk $argv $next
+            else if test $allow_visit_one_small_cave_twice = true -a $next != start
+                walk false $argv[2..] $next
             end
         end
     end
 end
 
-walk start
+walk $initial_allow_visit_one_small_cave_twice start
 
 echo $count
