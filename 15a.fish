6e98c586a64b563f1b15aef808393df3f8c6f4b7
Simon Lydell <simon.lydell@gmail.com>
2021-12-16 23:26:30 +0100

    Day 15 part 2
    It takes 5 minutes to generate the DOT graph, 5 seconds for the
    `dijkstra` program to find the shortest path.

--- 15a.fish
+++ 15a.fish
@@ -1,8 +1,15 @@
 source (status dirname)/helpers.fish
 
+set scale $argv[1]
+if not set -q scale[1]
+    set scale 1
+end
+
 set lines (cat)
 set width (string length $lines[1])
 set height (count $lines)
+set full_width (math $width \* $scale)
+set full_height (math $height \* $scale)
 
 function neighbors -a x y
     set xp (math $x - 1)
@@ -12,10 +19,10 @@ function neighbors -a x y
     if test $yp -ge 1
         echo $x $yp
     end
-    if test $xn -le $width
+    if test $xn -le $full_width
         echo $xn $y
     end
-    if test $yn -le $height
+    if test $yn -le $full_height
         echo $x $yn
     end
     if test $xp -ge 1
@@ -26,16 +33,25 @@ end
 function dot
     echo 'graph {'
     for y in (seq $height)
+        echo -n 1>&2 \r$y/$height
         set line (string split '' $lines[$y])
         for x in (seq $width)
             set n $line[$x]
-            neighbors $x $y | while read x2 y2
-                echo n"$x2"_"$y2" -- n"$x"_"$y" "[len=$n]"
+            for yi in (seq $scale)
+                for xi in (seq $scale)
+                    set xn (math "$x + ($xi - 1) * $width")
+                    set yn (math "$y + ($yi - 1) * $height")
+                    set nn (math "($n + $xi + $yi - 3) % 9 + 1")
+                    neighbors $xn $yn | while read x2 y2
+                        echo n"$x2"_"$y2" -- n"$xn"_"$yn" "[len=$nn]"
+                    end
+                end
             end
         end
     end
     echo '}'
+    echo 1>&2
 end
 
 # `dijkstra` comes from the `graphviz` package.
-dot | dijkstra -d n1_1 | string match -r n"$width"_"$height\s+\[dist=(\d+)" | tail -1
+dot | dijkstra -d n1_1 | string match -r n"$full_width"_"$full_height\s+\[dist=(\d+)" | tail -1
