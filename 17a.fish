5c9f372c3137ad4406755fdb92703da849d7d920
Simon Lydell <simon.lydell@gmail.com>
2021-12-17 08:56:03 +0100

    Clean up Day 17 part 1

--- 17a.fish
+++ 17a.fish
@@ -14,41 +14,13 @@ echo target x $target_x1 $target_x2 : $target_y1 $target_y2
 # should be the maximum possible velocity that also hits 0 (the starting point).
 # So we can start from the end and work out what `y` will be when the velocity
 # hits 0 (the peak).
-if test (math "abs($target_y1)") -gt (math "abs($target_y2)")
-    set y $target_y1
-else
-    set y $target_y2
-end
+set y $target_y1
 set vy $y
-
-echo start $y / $vy
 
 # It’s probably possible to do this with math, but my code brain was quicker than my math brain.
 while test $vy != 0
     set y (math $y - $vy)
     set vy (math $vy + 1)
-    echo step $y / $vy
 end
 
 echo $y
-exit
-
-# To see how the example best starting velocities fare:
-set x 0
-set y 0
-set vx 6
-set vy 9
-
-echo start $x,$y / $vx,$vy
-
-while not test $x -ge $target_x1 -a $x -le $target_x2 -a $y -ge $target_y1 -a $y -le $target_y2
-    set x (math $x + $vx)
-    set y (math $y + $vy)
-    if test $vx -lt 0
-        set vx (math $vx + 1)
-    else if test $vx -gt 0
-        set vx (math $vx - 1)
-    end
-    set vy (math $vy - 1)
-    echo $x,$y / $vx,$vy
-end
