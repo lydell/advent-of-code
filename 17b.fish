9fee4162bbb1638cdf86fa1456fb1220d22a6d3d
Simon Lydell <simon.lydell@gmail.com>
2021-12-17 12:36:36 +0100

    Parallelize Day 17 part 2 from 2 minutes to 20 seconds

--- 17b.fish
+++ 17b.fish
@@ -1,10 +1,39 @@
-read line
+set line $argv[1]
+if set -q line[1]
+    set -e argv[1]
+else
+    read line
+end
+
 set values (string match -r 'x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)' $line)
 set target_x1 $values[2]
 set target_x2 $values[3]
 set target_y1 $values[4]
 set target_y2 $values[5]
-echo target x $target_x1 $target_x2 : $target_y1 $target_y2
+
+if not set -q argv[1]
+    set max_start_vy (math "-$target_y1 - 1") # Shoot upwards to maximum height (see part 1).
+    set min_start_vy $target_y1 # Shoot downwards to lowest point in one step.
+    set max_start_vx $target_x2 # Shoot to the right-most point in one step.
+
+    set vx 0
+    set x 0
+    set i 0
+    while test $x -lt $target_x1
+        set i (math $i + 1)
+        set x (math $x + $i)
+    end
+    set min_start_vx $i # Shoot as weakly as possible to hit as far left in the target as possible.
+
+    # Now just test all combinations of velocities within the ranges.
+    set candidates (seq $min_start_vx $max_start_vx):(seq $min_start_vy $max_start_vy)
+    set length (count $candidates)
+    set threads (parallel --number-of-threads)
+    set max_args (math "ceil($length / $threads)")
+
+    string join \n $candidates | parallel --max-args=$max_args fish (status filename) "'$line'" | wc -l | string trim
+    exit
+end
 
 function hits_target -a vx vy
     set -l x 0
@@ -25,36 +54,8 @@ function hits_target -a vx vy
     return 1
 end
 
-set y $target_y1
-set vy $y
-
-set max_start_vy (math "-$y - 1") # Shoot upwards to maximum height (see part 1).
-set min_start_vy $y # Shoot downwards to lowest point in one step.
-
-set max_start_vx $target_x2 # Shoot to the right-most point in one step.
-
-set vx 0
-set x 0
-set i 0
-while test $x -lt $target_x1
-    set i (math $i + 1)
-    set x (math $x + $i)
-end
-set min_start_vx $i # Shoot as weakly as possible to hit as far left in the target as possible.
-
-echo vx $min_start_vx '->' $max_start_vx / vy $min_start_vy '->' $max_start_vy
-
-# Now just test all combinations of velocities within the ranges.
-set candidates (seq $min_start_vx $max_start_vx):(seq $min_start_vy $max_start_vy)
-set length (count $candidates)
-set count 0
-set i 0
-for item in $candidates
-    set i (math $i + 1)
-    echo -n 1>&2 \r$i/$length
+for item in $argv
     if hits_target (string split : $item)
-        set count (math $count + 1)
+        echo $item
     end
 end
-echo
-echo $count
