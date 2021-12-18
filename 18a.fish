166055f985cbbdc036b47b797da4085e7b547f6b
Simon Lydell <simon.lydell@gmail.com>
2021-12-18 13:50:39 +0100

    Optimize Day 18
    Part 2 now takes 4 minutes instead of 8.5 minutes.

--- 18a.fish
+++ 18a.fish
@@ -27,86 +27,96 @@ switch $part
         end
 end
 
+function explode -a i
+    set -l number $argv[2..]
+    set -l left $number[(math $i + 1)]
+    set -l right $number[(math $i + 2)]
+    set -l new_i $i
+    set -l nesting_delta 0
+    set -l final_nesting_delta 0
+    set -l item
+
+    for j in (seq (math $i - 1) 1)
+        set item $number[$j]
+        switch $item
+            case [
+                set nesting_delta (math $nesting_delta - 1)
+            case ]
+                set nesting_delta (math $nesting_delta + 1)
+            case '*'
+                set -l n (math $item + $left)
+                set number[$j] $n
+                if test $n -gt 9
+                    set new_i $j
+                    set final_nesting_delta $nesting_delta
+                end
+                break
+        end
+    end
+
+    for j in (seq (math $i + 4) (count $number))
+        set item $number[$j]
+        switch $item
+            case [
+            case ]
+            case '*'
+                set number[$j] (math $item + $right)
+                break
+        end
+    end
+
+    string join \n -- $new_i $final_nesting_delta $number[1..(math $i - 1)] 0 $number[(math $i + 4)..]
+end
+
 function reduce
     set -l number $argv
-    # echo 1>&2 addition $number
+    set -l count (count $number)
+    set -l item
+    set -l nesting 0
+    set -l i 1
 
-    while true
-        set -l nesting 0
-        set -l exploded false
-
-        for i in (seq (count $number))
-            set item $number[$i]
-            switch $item
-                case [
+    while test $i -le $count
+        set item $number[$i]
+        switch $item
+            case [
+                if test $nesting -lt 4
                     set nesting (math $nesting + 1)
-                    if test $nesting -gt 4
-                        # if test $nesting = 5
-                        set -l left $number[(math $i + 1)]
-                        set -l right $number[(math $i + 2)]
-                        # if test $left = [ -o $right = [
-                        #     continue
-                        # end
-                        for j in (seq (math $i - 1) 1)
-                            set item2 $number[$j]
-                            switch $item2
-                                case [
-                                case ]
-                                case '*'
-                                    # echo 1>&2 left set $number[$j] + $left at $j
-                                    # echo 1>&2 '  =' (math $item2 + $left)
-                                    set number[$j] (math $item2 + $left)
-                                    break
-                            end
-                        end
-                        for j in (seq (math $i + 4) (count $number))
-                            set item2 $number[$j]
-                            switch $item2
-                                case [
-                                case ]
-                                case '*'
-                                    # echo 1>&2 right set $number[$j] + $right at $j
-                                    # echo 1>&2 '  =' (math $item2 + $right)
-                                    set number[$j] (math $item2 + $right)
-                                    break
-                            end
-                        end
-                        set number $number[1..(math $i - 1)] 0 $number[(math $i + 4)..]
-                        set exploded true
-                        # echo 1>&2 explode $number
-                        break
+                else
+                    set number (explode $i $number)[3..]
+                    set count (count $number)
+                end
+            case ]
+                set nesting (math $nesting - 1)
+        end
+        set i (math $i + 1)
+    end
+
+    set nesting 0
+    set i 1
+
+    while test $i -le $count
+        set item $number[$i]
+        switch $item
+            case [
+                set nesting (math $nesting + 1)
+            case ]
+                set nesting (math $nesting - 1)
+            case '*'
+                if test $item -gt 9
+                    set number $number[1..(math $i - 1)] [ (math "floor($item / 2)") (math "ceil($item / 2)") ] $number[(math $i + 1)..]
+                    if test $nesting -lt 4
+                        set nesting (math $nesting + 1)
+                        set count (count $number)
+                    else
+                        set exploded (explode $i $number)
+                        set i (math $exploded[1] - 1)
+                        set nesting (math $nesting + $exploded[2])
+                        set number $exploded[3..]
+                        set count (count $number)
                     end
-                case ]
-                    set nesting (math $nesting - 1)
-            end
+                end
         end
-
-        if test $exploded = true
-            continue
-        end
-
-        set -l split false
-
-        for i in (seq (count $number))
-            set item $number[$i]
-            switch $item
-                case [
-                case ]
-                case '*'
-                    if test $item -gt 9
-                        set number $number[1..(math $i - 1)] [ (math "floor($item / 2)") (math "ceil($item / 2)") ] $number[(math $i + 1)..]
-                        set split true
-                        # echo 1>&2 split $number
-                        break
-                    end
-            end
-        end
-
-        if test $split = true
-            continue
-        end
-
-        break
+        set i (math $i + 1)
     end
 
     string join \n $number
@@ -132,25 +142,15 @@ function parse -a line
     string split '' (string replace -a , '' $line)
 end
 
-# echo (reduce [ (parse [[[[4,3],4],4],[7,[[8,4],9]]]) (parse [1,1]) ])
-# exit
-
 switch $part
     case a
         set sum (parse $lines[1])
         for i in (seq 2 $count)
             echo -n 1>&2 \r$i/$count
-            # echo ' ' $sum
-            # echo + (parse $lines[$i])
-            # echo \# [ $sum (parse $lines[$i]) ]
             set sum (reduce [ $sum (parse $lines[$i]) ])
-            # echo = $sum
-            # echo
-            # exit
         end
         echo
-        echo sum $sum
-        echo magnitude (magnitude $sum)
+        magnitude $sum
 
     case b
         for i in $argv
