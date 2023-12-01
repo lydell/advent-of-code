f90bc070126b8555ad50230b6c2b506b63adfa8b
Simon Lydell <simon.lydell@gmail.com>
2023-12-01 09:51:05 +0100

    Day 01: Use stdin instead of files

--- 2023/01/a.fish
+++ 2023/01/a.fish
@@ -1,1 +1,1 @@
-paste $argv[1] $argv[1] | string replace -r '^\D*(\d).+(\d)\D*$' '$1$2' | string join + | math
+cat | string replace -r '.+' '$0$0' | string replace -r '^\D*(\d).+(\d)\D*$' '$1$2' | string join + | math
