471c650e4ab236554fa6d01f14d8c2b2f099123b
Simon Lydell <simon.lydell@gmail.com>
2023-12-01 09:46:27 +0100

    Shorten day 01

--- 2023/01/a.fish
+++ 2023/01/a.fish
@@ -1,2 +1,1 @@
-set file $argv[1]
-rev $file | paste $file - | string replace -r '\D*(\d)\S*\t\D*(\d)\S*' '$1$2' | string join + | math
+paste $argv[1] $argv[1] | string replace -r '^\D*(\d).+(\d)\D*$' '$1$2' | string join + | math
