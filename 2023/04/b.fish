5ef2a895749c86e45e6c5146d16beee402238f5a
Simon Lydell <simon.lydell@gmail.com>
2023-12-04 07:42:03 +0100

    Inline b.py

--- 2023/04/b.fish
+++ 2023/04/b.fish
@@ -5,6 +5,10 @@ begin
         | string replace -r ' +\| +' '}&{' \
         | string replace -r '$' '}),' \
         | string replace -ar ' +' ','
-    echo ']'
-    tail -n +2 b.py
+    echo ']
+num_cards = [1 for _ in cards]
+for i, wins in enumerate(cards):
+    for j in range(i + 1, i + 1 + wins):
+        num_cards[j] += num_cards[i]
+print(sum(num_cards))'
 end | python
--- 2023/04/b.py
+++
@@ -1,6 +0,0 @@
-cards = [] # replaced by b.fish
-num_cards = [1 for _ in cards]
-for i, wins in enumerate(cards):
-    for j in range(i + 1, i + 1 + wins):
-        num_cards[j] += num_cards[i]
-print(sum(num_cards))
