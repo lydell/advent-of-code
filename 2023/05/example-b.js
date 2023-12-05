a7a992dd93d921ed534a6d34eb29e72d38a7077d
Simon Lydell <simon.lydell@gmail.com>
2023-12-05 08:48:09 +0100

    Day 05

---
+++ 2023/05/example-b.js
@@ -0,0 +1,38 @@
+seeds = Array.from("79 14 55 13".matchAll(/(\d+) (\d+)/g), m => [Number(m[1]), Number(m[1]) + Number(m[2]) - 1])
+function run(ranges) {
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 98 && range[0] < 98 + 2) { range[0] = 50 + range[0] - 98; if (range[1] < 98 + 2) { range[1] = 50 + range[1] - 98 } else { ranges.push([98 + 2, range[1]]); range[1] = 50 + 2 - 1 } }
+else if (range[0] >= 50 && range[0] < 50 + 48) { range[0] = 52 + range[0] - 50; if (range[1] < 50 + 48) { range[1] = 52 + range[1] - 50 } else { ranges.push([50 + 48, range[1]]); range[1] = 52 + 48 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 15 && range[0] < 15 + 37) { range[0] = 0 + range[0] - 15; if (range[1] < 15 + 37) { range[1] = 0 + range[1] - 15 } else { ranges.push([15 + 37, range[1]]); range[1] = 0 + 37 - 1 } }
+else if (range[0] >= 52 && range[0] < 52 + 2) { range[0] = 37 + range[0] - 52; if (range[1] < 52 + 2) { range[1] = 37 + range[1] - 52 } else { ranges.push([52 + 2, range[1]]); range[1] = 37 + 2 - 1 } }
+else if (range[0] >= 0 && range[0] < 0 + 15) { range[0] = 39 + range[0] - 0; if (range[1] < 0 + 15) { range[1] = 39 + range[1] - 0 } else { ranges.push([0 + 15, range[1]]); range[1] = 39 + 15 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 53 && range[0] < 53 + 8) { range[0] = 49 + range[0] - 53; if (range[1] < 53 + 8) { range[1] = 49 + range[1] - 53 } else { ranges.push([53 + 8, range[1]]); range[1] = 49 + 8 - 1 } }
+else if (range[0] >= 11 && range[0] < 11 + 42) { range[0] = 0 + range[0] - 11; if (range[1] < 11 + 42) { range[1] = 0 + range[1] - 11 } else { ranges.push([11 + 42, range[1]]); range[1] = 0 + 42 - 1 } }
+else if (range[0] >= 0 && range[0] < 0 + 7) { range[0] = 42 + range[0] - 0; if (range[1] < 0 + 7) { range[1] = 42 + range[1] - 0 } else { ranges.push([0 + 7, range[1]]); range[1] = 42 + 7 - 1 } }
+else if (range[0] >= 7 && range[0] < 7 + 4) { range[0] = 57 + range[0] - 7; if (range[1] < 7 + 4) { range[1] = 57 + range[1] - 7 } else { ranges.push([7 + 4, range[1]]); range[1] = 57 + 4 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 18 && range[0] < 18 + 7) { range[0] = 88 + range[0] - 18; if (range[1] < 18 + 7) { range[1] = 88 + range[1] - 18 } else { ranges.push([18 + 7, range[1]]); range[1] = 88 + 7 - 1 } }
+else if (range[0] >= 25 && range[0] < 25 + 70) { range[0] = 18 + range[0] - 25; if (range[1] < 25 + 70) { range[1] = 18 + range[1] - 25 } else { ranges.push([25 + 70, range[1]]); range[1] = 18 + 70 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 77 && range[0] < 77 + 23) { range[0] = 45 + range[0] - 77; if (range[1] < 77 + 23) { range[1] = 45 + range[1] - 77 } else { ranges.push([77 + 23, range[1]]); range[1] = 45 + 23 - 1 } }
+else if (range[0] >= 45 && range[0] < 45 + 19) { range[0] = 81 + range[0] - 45; if (range[1] < 45 + 19) { range[1] = 81 + range[1] - 45 } else { ranges.push([45 + 19, range[1]]); range[1] = 81 + 19 - 1 } }
+else if (range[0] >= 64 && range[0] < 64 + 13) { range[0] = 68 + range[0] - 64; if (range[1] < 64 + 13) { range[1] = 68 + range[1] - 64 } else { ranges.push([64 + 13, range[1]]); range[1] = 68 + 13 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 69 && range[0] < 69 + 1) { range[0] = 0 + range[0] - 69; if (range[1] < 69 + 1) { range[1] = 0 + range[1] - 69 } else { ranges.push([69 + 1, range[1]]); range[1] = 0 + 1 - 1 } }
+else if (range[0] >= 0 && range[0] < 0 + 69) { range[0] = 1 + range[0] - 0; if (range[1] < 0 + 69) { range[1] = 1 + range[1] - 0 } else { ranges.push([0 + 69, range[1]]); range[1] = 1 + 69 - 1 } }
+
+for (range of ranges) if (false) {}
+else if (range[0] >= 56 && range[0] < 56 + 37) { range[0] = 60 + range[0] - 56; if (range[1] < 56 + 37) { range[1] = 60 + range[1] - 56 } else { ranges.push([56 + 37, range[1]]); range[1] = 60 + 37 - 1 } }
+else if (range[0] >= 93 && range[0] < 93 + 4) { range[0] = 56 + range[0] - 93; if (range[1] < 93 + 4) { range[1] = 56 + range[1] - 93 } else { ranges.push([93 + 4, range[1]]); range[1] = 56 + 4 - 1 } }
+return ranges
+}
+console.log(Math.min(...run(seeds).map(range => range[0])))
+
