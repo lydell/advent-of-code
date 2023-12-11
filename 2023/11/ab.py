2e040afa18abdcb53e41f38cab5451c3a647d3cc
Simon Lydell <simon.lydell@gmail.com>
2023-12-11 08:55:10 +0100

    Day 11

---
+++ 2023/11/ab.py
@@ -0,0 +1,50 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("You must pass the amount of expansion as the first argument")
+    print("Part 1: 2, Part 2: 1000000")
+    exit(1)
+
+expansion = int(sys.argv[1])
+
+map = [list(line.strip()) for line in sys.stdin]
+
+height = len(map)
+width = len(map[0])
+
+empty_columns = set()
+empty_rows = set()
+
+for y, row in enumerate(map):
+    if all(cell == '.' for cell in row):
+        empty_rows.add(y)
+
+for x in range(width):
+    if all(row[x] == '.' for row in map):
+        empty_columns.add(x)
+
+galaxies = []
+ax = -1
+ay = -1
+
+for y, row in enumerate(map):
+    ay += 1
+    if y in empty_rows:
+        ay += expansion - 1
+        continue
+    ax = -1
+    for x, cell in enumerate(row):
+        ax += 1
+        if x in empty_columns:
+            ax += expansion - 1
+            continue
+        if cell == '#':
+            galaxies.append((ay, ax))
+
+def taxicab_distance(y1, x1, y2, x2):
+    return abs(y1 - y2) + abs(x1 - x2)
+
+print(sum(
+    sum(taxicab_distance(y, x, y2, x2) for (y2, x2) in galaxies[i + 1:])
+    for i, (y, x) in enumerate(galaxies[:-1])
+))
