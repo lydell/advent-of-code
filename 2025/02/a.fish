ffbb0660b63680c499f58fb208ec6b76ec37ff29
Simon Lydell <simon.lydell@gmail.com>
2025-12-02 11:13:52 +0100

    Day 02 part 2

--- 2025/02/a.fish
+++ 2025/02/a.fish
@@ -1,3 +1,4 @@
+set repeat $argv[1]
 cat | string split , | while read --delimiter - from to
     seq -f "%.0f" $from $to
-end | rg --pcre2 '^(\d+)\1$' | string join + | math
+end | rg --pcre2 "^(\d+)\1$repeat\$" | string join + | math
---
+++ 2025/02/b.fish
@@ -0,0 +1,1 @@
+fish a.fish +
