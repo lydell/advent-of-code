ec170e3735ea17c96bb37f8e0fdb6a9fbd427056
Simon Lydell <simon.lydell@gmail.com>
2025-12-04 08:49:00 +0100

    Day 04 part 2

---
+++ 2025/04/b.mjs
@@ -0,0 +1,47 @@
+import fs from "node:fs";
+
+const lines = fs.readFileSync(process.stdin.fd, "utf-8").trim().split("\n");
+
+const height = lines.length;
+const width = lines[0].length;
+
+const map = new Map();
+
+for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+        map.set(`${x},${y}`, lines[x][y]);
+    }
+}
+
+let count = 0;
+
+while (true) {
+    let moved = false;
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (map.get(`${x},${y}`) === "@") {
+                const neighbors = [
+                    [-1, -1],
+                    [0, -1],
+                    [1, -1],
+                    [-1, 0],
+                    [1, 0],
+                    [-1, 1],
+                    [0, 1],
+                    [1, 1],
+                ].map(([dx, dy]) => map.get(`${x+dx},${y+dy}`));
+                const rolls = neighbors.filter(d => d === "@");
+                if (rolls.length < 4) {
+                    count++;
+                    moved = true;
+                    map.set(`${x},${y}`, ".");
+                }
+            }
+        }
+    }
+    if (!moved) {
+        break;
+    }
+}
+
+console.log(count);
