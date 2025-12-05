c35edc806e90547f7f5959c27cbfcf70b2a602f2
Simon Lydell <simon.lydell@gmail.com>
2025-12-05 08:49:08 +0100

    Day 05 part 1

---
+++ 2025/05/a.mjs
@@ -0,0 +1,18 @@
+import fs from "node:fs";
+
+const [section1, section2] = fs.readFileSync(process.stdin.fd, "utf-8")
+    .trim()
+    .split("\n\n")
+    ;
+
+const ranges = section1.split("\n").map(line => {
+    const [from, to] = line.split("-");
+    return [Number(from), Number(to)];
+});
+
+const numFresh = section2.split("\n").filter(line => {
+    const num = Number(line);
+    return ranges.some(([from, to]) => num >= from && num <= to);
+}).length;
+
+console.log(numFresh);
